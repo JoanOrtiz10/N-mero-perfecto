4fb79bb90681ad300fe4a48212746cdcf5aa7839
JoanOrtiz10 <ortizjoan404@gmail.com>
2024-08-22 15:44:39 -0500

    comparacion de codigos

--- NumberPerfect/src/numberperfect/NumberPerfect.java
+++ NumberPerfect/src/numberperfect/NumberPerfect.java
@@ -1,37 +1,45 @@
 package numberperfect;
+import java.util.Scanner;
 
-/**
- *
- * @author BYzYzzCODE
- */
+
 public class NumberPerfect {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        long startTime = System.nanoTime();
-        int number = 6;
-        String result = validationNumber(number);
-        System.out.println(result);
-        long endTime = System.nanoTime()- startTime;
-        System.out.println("tiemp: " + endTime);
-    }
-
-    public static String validationNumber(int num) {
-
-        int sum = 0;
-        for (int i = 1; i <= num / 2; i++) {
-            if (num % i == 0) {
-                sum += i;
+       int i, suma = 0;
+        Scanner sc = new Scanner(System.in);
+        
+     
+        long empiezaTiempo = System.currentTimeMillis();
+        
+        System.out.print("Ingrese numero perfecto: ");
+        int numero = sc.nextInt();
+      
+        for (i = 1; i < numero; i++) {                             
+            if (numero % i == 0) {
+                suma = suma + i;   
             }
         }
+
+        if (suma == numero) {
+            System.out.println("Es perfecto");
+        } else {
+            System.out.println("No es Perfecto");
+        }
         
-        if(num == sum ){
-        return "es un numero perfecto" ;
-        }else{
-        return "no es un numero perfecto";
+        
+        long finTiempo = System.currentTimeMillis();
+        
+        
+        long duracion = finTiempo - empiezaTiempo;
+        
+       
+         System.out.println("Tiempo de ejecucion: " + duracion + " ms");
+        
+       
         }
     }
 
-}
+
